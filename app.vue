4a124461317cebd69f783a0526d3d25eddcfba3c
azabroflovski <azabroflovski@gmail.com>
2021-11-28 15:08:50 +0500

    feat(ui): add button component

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <app-button label="Request Beta" />
   </div>
 </template>
+
+<script>
+  import AppButton from './components/base/AppButton'
+
+  export default {
+    components: {
+      AppButton
+    }
+  }
+</script>
---
+++ components/base/AppButton.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="app-btn app-btn__default" :class="buttonClassNames">
+    {{ label }}
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      label: {
+        type: String,
+      },
+
+      rounded: {
+        type: Boolean,
+      },
+    },
+
+    computed: {
+      buttonClassNames() {
+        return {
+          'app-btn__rounded': this.rounded
+        }
+      }
+    },
+  }
+</script>
+
+<style lang="scss">
+  .app-btn {
+    display: inline-flex;
+    background-color: transparent;
+    border: none;
+    user-select: none;
+    padding: 12px 32px;
+    border-radius: 6px;
+    font-weight: 400;
+
+    &:hover {
+      cursor: pointer;
+   }
+  }
+
+
+  // Button types
+  .app-btn {
+    &__default {
+      background-color: #000;
+      color: #fff;
+    }
+  }
+
+  // Button variants
+  .app-btn {
+    &__rounded {
+      border-radius: 26px;
+    }
+  }
+</style>
